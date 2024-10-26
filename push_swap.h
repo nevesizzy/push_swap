fb2da1554cb6deb2a699f742f9c13a6f61bf891d
Isabella <126694876+nevesizzy@users.noreply.github.com>
2024-10-26 14:54:14 +0100

    Update push_swap.h

--- push_swap.h
+++ push_swap.h
@@ -16,17 +16,17 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <stdio.h>
-#include <stdbool.h>
 
 # define INTMAX 2147483647
 # define INTMIN -2147483648
-typedef struct s_stack_node
+
+typedef struct s_list
 {
     int value;
-    struct s_stack_node *next;
+    struct s_list *next;
 } t_list;
 
-// Operações básicas
+// Operações de pilha
 void sa(t_list **a);
 void sb(t_list **b);
 void ss(t_list **a, t_list **b);
@@ -39,13 +39,17 @@ void rra(t_list **a);
 void rrb(t_list **b);
 void rrr(t_list **a, t_list **b);
 
-// Algoritmo de ordenação
+// Algoritmos de ordenação
 void sort_stack(t_list **a, t_list **b);
+void tiny_sort(t_list **a);
+void quick_sort_partition(t_list **a, t_list **b, int pivot);
+void radix_sort(t_list **a, t_list **b);
 
 // Funções utilitárias
 t_list *create_node(int value);
 void free_stack(t_list **stack);
-void print_stack(t_list *stack);
 int is_sorted(t_list *stack);
 int stack_size(t_list *stack);
+int find_max_value(t_list *stack);
+
 #endif
